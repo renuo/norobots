a42e21788c184562759ab3a45b093af6b24a7ddc
Etienne Kyburz <etienne.kyburz@renuo.ch>
2025-05-21 08:21:17 +0200

    Correct insert of middleware

--- lib/norobots/rails.rb
+++ lib/norobots/rails.rb
@@ -3,7 +3,7 @@
 module Norobots
   class Railtie < Rails::Railtie
     initializer "norobots.configure_rack_middleware" do |app|
-      app.middleware.insert_before Rack::Sendfile, Norobots::Middleware if ENV["BLOCK_ROBOTS"] || ENV["CANONICAL_HOST"]
+      app.middleware.insert_before Rack::Sendfile, Norobots::Middleware unless ENV["BLOCK_ROBOTS"].nil? || ENV["CANONICAL_HOST"].nil?
     end
   end
 end
